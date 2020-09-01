2ee01f8bf0da4c550fec1c361fa1f3682d60deaa
arturjzapater <zapater.artur@gmail.com>
2020-09-01 10:24:42 +0200

    Server changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jsonServer = require('json-server')
 const path = require('path')
 
 const { PORT } = process.env
@@ -6,5 +7,6 @@ const { PORT } = process.env
 const app = express()
 
 app.use(express.static(path.resolve('./client/build')))
+app.use('/api', jsonServer.router('db.json'));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
