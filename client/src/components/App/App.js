096e62f701b7deef3074dce28782565d6efaea67
Christian Davidov <chris.davidov@gmail.com>
2020-09-02 16:26:32 +0200

    Finished for the day

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import './App.css'
-import Nav from '../Nav/Nav.js'
+import './App.scss'
+import Nav from '../Nav/Nav'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import About from '../About/About.js'
 import Profile from '../Profile/Profile'
@@ -8,14 +8,17 @@ import Search from '../Search/Search'
 
 function App () {
   return (
+    <>
     <Router>
-      <Nav authenticated={true} />
+      <Nav authenticated={true}/>
+      <li>joe</li>
       <Switch>
         <Route path='/about' exact component={About}/>
         <Route path='/profile' component={Profile} />
         <Route path='/search' component={Search} />
       </Switch>
     </Router>
+    </>
   )
 }
 
