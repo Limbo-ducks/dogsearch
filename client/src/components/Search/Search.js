1f0f10c18a0e9b52cf1b0be0f7d89a9cab3ada37
marie winther madsen <mariewmadsen@gmail.com>
2020-09-02 16:40:39 +0200

    added mongodb

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -2,20 +2,28 @@ import React, { useState, useEffect } from 'react'
 import ResultList from './ResultList'
 import Filter from './Filter'
 
+const makeOpts = (body, method = 'GET') => ({
+  method,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body
+})
+
 const Search = () => {
-  const baseURL = '/api/profiles'
+  const baseURL = '/api/search'
   const [status, setStatus] = useState('idle')
   const [hits, setHits] = useState([])
   const [searchQuery, setSearchQuery] = useState('')
 
   const search = query => {
-    setSearchQuery(query)
+    setSearchQuery(JSON.stringify(query))
     setStatus('loading')
   }
 
   useEffect(() => {
     if (status === 'loading') {
-      fetch(`${baseURL}?${searchQuery}`)
+      fetch(baseURL, makeOpts(searchQuery, 'POST'))
         .then(res => res.json())
         .then(data => setHits(data))
         .catch(console.log)
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
-const jsonServer = require('json-server')
+const bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb')
 const path = require('path')
+const searchController = require('./src/controllers/searchController')
 
 const { PORT = 5000 } = process.env
+const mongoUri = 'mongodb://localhost:27017'
+const mongoOpts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
 
 const app = express()
+const client = new MongoClient(mongoUri, mongoOpts)
 
-app.use(express.static(path.resolve('./client/build')))
-app.use('/api', jsonServer.router('db.json'))
+client.connect()
+    .then(client => {
+        app.use(bodyParser.json())
+        app.use(express.static(path.resolve('./client/build')))
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+        app.use((req, res, next) => console.log('Request body: ', req.body) || next())
+
+        app.post('/api/search', searchController(client))
+
+        app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+    })
+    .catch(console.error)
