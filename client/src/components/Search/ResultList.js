fa5df014512a66d62b2997ae6fdfc13ce9979e73
arturjzapater <zapater.artur@gmail.com>
2020-09-03 14:48:12 +0200

    added pagination

--- client/src/components/Search/ResultList.js
+++ client/src/components/Search/ResultList.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ResultCard from './ResultCard'
+import Pagination from '@material-ui/lab/Pagination'
 
 function ResultList (props) {
+  const [page, setPage] = useState(1);
+
+  const count = props.data.length
+  const pages = Math.ceil(count / 16)
+  const start = 16 * (page - 1)
+  const end = 16 * page
+
   return (
     <div>
-      {props.data.map(profile => {
+      {props.data
+      .slice(start, end)
+      .map(profile => {
         return <ResultCard {...profile} key={JSON.stringify(profile)} />
       })}
+      <Pagination count={pages} onChange={(_, p) => setPage(p)} />
     </div>
   )
 }
