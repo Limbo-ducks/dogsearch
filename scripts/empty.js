e06586616d5e8c35e9295030d48322cb64da6a5b
arturjzapater <zapater.artur@gmail.com>
2020-09-03 15:33:38 +0200

    created script to empty db

---
+++ scripts/empty.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb')
+
+const mongoUri = 'mongodb://localhost:27017'
+const mongoOpts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+const client = new MongoClient(mongoUri, mongoOpts)
+
+client.connect()
+    .then(client => client.db('talentwyre').collection('profiles'))
+    .then(col => col.deleteMany({}))
+    .then(() => console.log('DB successfully emptied'))
+    .catch(console.error)
+    .finally(() => client.close())
