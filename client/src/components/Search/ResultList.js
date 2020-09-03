ce6859768cc2a09da693575dbecd105068b38ea0
arturjzapater <zapater.artur@gmail.com>
2020-09-03 15:14:42 +0200

    get different random images

--- client/src/components/Search/ResultList.js
+++ client/src/components/Search/ResultList.js
@@ -1,9 +1,9 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import ResultCard from './ResultCard'
 import Pagination from '@material-ui/lab/Pagination'
 
 function ResultList (props) {
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1)
 
   const count = props.data.length
   const pages = Math.ceil(count / 16)
@@ -13,10 +13,10 @@ function ResultList (props) {
   return (
     <div>
       {props.data
-      .slice(start, end)
-      .map(profile => {
-        return <ResultCard {...profile} key={JSON.stringify(profile)} />
-      })}
+        .slice(start, end)
+        .map(profile => {
+          return <ResultCard {...profile} key={JSON.stringify(profile)} />
+        })}
       <Pagination count={pages} onChange={(_, p) => setPage(p)} />
     </div>
   )
