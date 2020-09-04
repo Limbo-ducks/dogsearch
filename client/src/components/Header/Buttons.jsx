59f49c11069647972c6d6cdab8d50da7480ff93d
Konrad Guzior <Guzior@Konrad.local>
2020-09-04 10:08:59 +0200

    Added Login and simple Signup component

--- client/src/components/Header/Buttons.jsx
+++ client/src/components/Header/Buttons.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Buttons.scss'
 
 const Buttons = () => {
   return (
     <section className="header__buttons">
-      <button className="header__buttons__button header__buttons__button--signup">sign-up</button>
-      <button className="header__buttons__button header__buttons__button--login">Log in</button>
+      <Link to='/signup'>
+        <button 
+          className="header__buttons__button header__buttons__button--signup">
+            sign-up</button>
+      </Link>
+      <Link to='/login'>
+        <button 
+          className="header__buttons__button header__buttons__button--login">
+            Log in</button>
+      </Link>
     </section>
   )
 }
