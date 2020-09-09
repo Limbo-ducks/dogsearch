6dee60bb1ff0132480da9f25caff54edc482156d
Konrad Guzior <Guzior@Konrad.local>
2020-09-09 10:22:09 +0200

    Added personal attributes

--- client/src/components/Profile/ProfileAttribute.jsx
+++ client/src/components/Profile/ProfileAttribute.jsx
@@ -4,11 +4,14 @@ const ProfileAttribute = ({data, label, unit ,odd = false}) => {
     return(
         <article className={`content__attributes__list__row ${odd?'content__attributes__list__row--odd':null}`}>
           <article>
-            <p>{label[0]}: {data[0]}{unit[0]}</p>
+            <p>{label[0]}: {Array.isArray(data[0]) ? data[0].join(' ,') : data[0]}{unit[0]}</p>
           </article>
-          <article>
-            <p>{label[1]}: {data[1]}{unit[1]}</p>
-          </article>
+          {data[1] ? 
+              <article>
+                 <p>{label[1]}: {Array.isArray(data[1]) ? data[1].join(' ,') : data[1]}{unit[1]}</p>
+              </article>
+            : null}
+         
         </article>
     )
 }
