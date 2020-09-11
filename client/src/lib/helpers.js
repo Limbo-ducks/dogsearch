97565b79bacfb68641305a6fd612fb3f6258fca0
marie winther madsen <mariewmadsen@gmail.com>
2020-09-11 13:09:59 +0200

    added user feedback on wrong input

--- client/src/lib/helpers.js
+++ client/src/lib/helpers.js
@@ -13,7 +13,7 @@ const map = fn => xs => Array.isArray(xs)
 const capitalise = str => str[0].toUpperCase() + str.slice(1)
 const spaciate = str => str.replace(/([a-z])([A-Z])/, (_, a, b) => `${a} ${b}`)
 
-const getAutocompleteValue = (options, value) => 
+const getAutocompleteValue = (options, value) =>
   Array.isArray(value)
     ? value.map(value => R.defaultTo(null, options.find(x => x.value === value)))
     : R.is(Object, value)
