452a1cd8bbaaad13beefaa01850708209d5dd502
Christian Davidov <chris.davidov@gmail.com>
2020-09-11 10:52:23 +0200

    Added fallback for audio section on profile

--- client/src/components/Profile/ProfileAudio.jsx
+++ client/src/components/Profile/ProfileAudio.jsx
@@ -6,7 +6,7 @@ import AudiotrackIcon from '@material-ui/icons/Audiotrack';
 const ProfileAudio = ({data}) => {
   return (
     <section className="content__audio">
-      <iframe title="vimeo-player" src={data.audio} width="100%" height="auto" frameborder="0" allowfullscreen></iframe>
+      {data.audio ? (<><iframe title="vimeo-player" src={data.audio} width="100%" height="auto" frameborder="0" allowfullscreen></iframe>
 
       <article className="content__description">
         <h4>Description:</h4>
@@ -19,7 +19,7 @@ const ProfileAudio = ({data}) => {
         <li><AudiotrackIcon /> Character reel</li>
         <li><AudiotrackIcon /> Commercial reel</li>
         <li><AudiotrackIcon /> Character reel</li>
-      </ul>
+      </ul></>) : <p>No Audio has been provided by the Talent.</p>}
 
     </section>
   )
